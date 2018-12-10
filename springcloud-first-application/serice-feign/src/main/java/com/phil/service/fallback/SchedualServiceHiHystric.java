9aebd91bc6b7e9932a2916d533f153a769c4e543
phil <980809846@qq.com>
2018-12-10 16:11:02 +0800

    完成初始ribbon、feign和hystrix等功能

---
+++ springcloud-first-application/serice-feign/src/main/java/com/phil/service/fallback/SchedualServiceHiHystric.java
@@ -0,0 +1,12 @@
+package com.phil.service.fallback;
+
+import com.phil.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry, you are fail,"+name;
+    }
+}
