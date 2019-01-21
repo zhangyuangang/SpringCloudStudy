b7cdc073b9e74d82549f45af1531c626c3ab5b1d
phil <980809846@qq.com>
2019-01-21 15:22:07 +0800

    hystrix监控

--- springcloud-first-application/service-feign/src/main/java/com/phil/controller/HiController.java
+++ springcloud-first-application/service-feign/src/main/java/com/phil/controller/HiController.java
@@ -2,13 +2,15 @@ package com.phil.controller;
 
 import com.phil.service.SchedualServiceHi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HiController {
-    
+
+    @Qualifier("service-hi")
     @Autowired
     SchedualServiceHi schedualServiceHi;
 
--- springcloud-first-application/service-hi/src/main/java/com/phil/ServiceHiApplication.java
+++ springcloud-first-application/service-hi/src/main/java/com/phil/ServiceHiApplication.java
@@ -1,18 +1,20 @@
 package com.phil;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -36,8 +38,19 @@ public class ServiceHiApplication {
     String port;
 
     @RequestMapping("/hi")
-    public String home(@RequestParam(value = "name", defaultValue = "phil") String name, HttpServletRequest request) {
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String home(@RequestParam(value = "name", defaultValue = "phil") String name) {
         return "hi " + name + " ,i am from port:" + port;
     }
 
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet(){
+        ServletRegistrationBean registration=new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        registration.addUrlMappings("/hystrix.stream");
+        return registration;
+    }
 }
