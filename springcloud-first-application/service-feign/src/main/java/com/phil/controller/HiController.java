14b47af91ab3aa3d25dfae3d29d8bfb4c41d4d35
phil <980809846@qq.com>
2019-01-23 10:49:54 +0800

    修复idea工程

--- springcloud-first-application/service-feign/src/main/java/com/phil/controller/HiController.java
+++ springcloud-first-application/service-feign/src/main/java/com/phil/controller/HiController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HiController {
 
-    @Qualifier("service-hi")
     @Autowired
     SchedualServiceHi schedualServiceHi;
 
